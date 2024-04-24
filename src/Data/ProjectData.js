371580667716fe7d05b3de9eacd836d9270482a5
akif470 <akifmansoori470@gmail.com>
2024-04-24 15:11:11 +0530

    image changes

--- src/Data/ProjectData.js
+++ src/Data/ProjectData.js
@@ -1,40 +1,52 @@
 import Project1 from '../assets/Projects/project1.png';
 import ProjectM1 from '../assets/Projects/projectM1.png';
-import Project6 from '../assets/Projects/project6.png';
-import ProjectM6 from '../assets/Projects/projectM6.png';
+import Project2 from '../assets/Projects/project2.png';
+import ProjectM2 from '../assets/Projects/projectM2.png';
 import Project3 from '../assets/Projects/project3.png';
 import ProjectM3 from '../assets/Projects/projectM3.png';
+import Project4 from '../assets/Projects/project4.png';
 import Project5 from '../assets/Projects/project5.png';
-import ProjectM5 from '../assets/Projects/projectM5.png';
+import Project6 from '../assets/Projects/project6.png';
 
 const ProjectData = [
   {
-    imgD: Project6,
-    imgM: ProjectM6,
-    heading: 'CRIBO Office Project',
-    span: 'JAVASCRIPT, HTML, CSS',
-    url: 'https://www.cribo.co.uk/',
+    imgD: Project1,
+    imgM: ProjectM1,
+    heading: 'Modern Solution Responsive UI',
+    span: 'JAVASCRIPT, HTML, CSS, SASS',
+    url: 'https://akif470.github.io/Modern-Responsive-Web-UI/',
   },
   {
     imgD: Project5,
-    imgM: ProjectM5,
-    heading: 'NestoHub, Office Project',
-    span: 'REACT.JS, TAILWIND-CSS',
-    url: 'https://www.nestohub.in/',
+    heading: 'Millionaire Quiz Game',
+    span: 'JAVASCRIPT, REACT.JS, CSS',
+    url: 'https://millionarequizgame343.netlify.app/',
   },
   {
-    imgD: Project1,
-    imgM: ProjectM1,
-    heading: 'Responsive UI for travels',
-    span: 'REACT.JS, CSS, BOOTSTRAP',
-    url: 'https://travelo77.netlify.app/',
+    imgD: Project2,
+    imgM: ProjectM2,
+    heading: 'Creative Agency Responsive UI',
+    span: 'JAVASCRIPT, HTML, CSS, SASS',
+    url: 'https://akif470.github.io/Creative-Agency-Responsive-UI/',
   },
   {
     imgD: Project3,
     imgM: ProjectM3,
-    heading: 'Responsive UI for Restaurants',
-    span: 'REACT.JS, CSS, BOOTSTRAP',
-    url: 'https://restaurant30.netlify.app/',
+    heading: 'Food Delivery App',
+    span: 'JAVASCRIPT, REACT.JS, CSS',
+    url: 'https://fooddeliveryapp233.netlify.app/',
+  },
+  {
+    imgD: Project4,
+    heading: 'Digital Solution UI',
+    span: 'JAVASCRIPT, HTML, CSS, SASS',
+    url: 'https://akif470.github.io/Digital-Solution-UI/',
+  },
+  {
+    imgD: Project6,
+    heading: 'Dice Game',
+    span: 'REACT.JS, CSS',
+    url: 'https://dicegame26.netlify.app/',
   },
 ];
 
--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -1,39 +1,43 @@
-import React from 'react'
-import './Project.css'
-import ProjectData from '../../Data/ProjectData'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import './Project.css';
+import ProjectData from '../../Data/ProjectData';
 
 const Project = () => {
-    return (
-        <div className="mYcontainer container project">
-            <div className="row">
-                {ProjectData.map((item) => {
-                    return (
-                        <div className="col-lg-6">
-                            <div className="pro-box" data-aos="flip-right"
-                                data-aos-easing="ease-out-cubic"
-                                data-aos-delay="300"
-                                data-aos-duration="1200">
-                                <img className="pro-img pro-desktop" src={item.imgD} />
-                                <img className="pro-img pro-mobile" src={item.imgM} />
-                                <div className='pro-content'>
-                                    <div className="pro-headings">
-                                        <h3 className="pro-heading">{item.heading}</h3>
-                                        <p className="pro-para"><span>{item.span}</span></p>
-                                    </div>
-                                    <div>
-                                        <a href={item.url} target='_blank'>
-                                            <button className='pro-btn'>live view</button>
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    )
-                })}
+  return (
+    <div className='mYcontainer container project'>
+      <div className='row'>
+        {ProjectData.map((item) => {
+          return (
+            <div className='col-lg-6'>
+              <div
+                className='pro-box'
+                data-aos='flip-right'
+                data-aos-easing='ease-out-cubic'
+                data-aos-delay='300'
+                data-aos-duration='1200'
+              >
+                <img className='pro-img pro-desktop' src={item.imgD} />
+                <img className='pro-img pro-mobile' src={item.imgM} />
+                <div className='pro-content'>
+                  <div className='pro-headings'>
+                    <h3 className='pro-heading'>{item.heading}</h3>
+                    <p className='pro-para'>
+                      <span>{item.span}</span>
+                    </p>
+                  </div>
+                  <div>
+                    <a href={item.url} target='_blank'>
+                      <button className='pro-btn'>live view</button>
+                    </a>
+                  </div>
+                </div>
+              </div>
             </div>
-        </div>
-    )
-}
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 
-export default Project
+export default Project;
